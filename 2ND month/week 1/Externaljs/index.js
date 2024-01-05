b4c310c4328370a8a31386416b939c277b27bf80
kendrickdave <mjabbay4real@gmail.com>
2024-01-05 13:36:45 +0100

    Update

---
+++ 2ND month/week 1/Externaljs/index.js
@@ -0,0 +1,12 @@
+ //get access to the button 
+ const button = document.querySelector('button');
+
+ //add  event to the button
+
+ button.addEventListener('click', showMyName);
+
+ // Function to show my name 
+
+ function showMyName() {
+    alert('My Name Abiodun Asogba');
+ }
