dbbd6f16b2ccf1e84b7d372354c5cad5eb0c5eae
Asogba Abiodun Kendrick <asogbaabiodun92@gmail.com>
2024-01-29 09:31:24 +0100

    update

--- 2ND month/Week1/functions/index.js
+++ 2ND month/Week1/functions/index.js
@@ -1,72 +1,97 @@
-let count = 0
+// // example 
+// function sum(){
+//     let result = 20 + 30 
+//     console.log(`The Result is ${result}`); 
+// }
 
-//function without parameters
-function addToCount() {
-    let localCount = 1
+// sum()
+// sum()
+// sum()
+// sum()
 
-    count = count + 1
-    localCount = localCount + 1
 
-    console.log("Count is: ", count)
-    console.log("Local Count is: ", localCount)
 
-}
+// let count = 0
 
-addToCount()
+// //function without parameters
+// function addToCount() {
+//     let localCount = 1
+
+//     count = count + 1
+//     localCount = localCount + 1
+
+//     console.log("Count is: ", count)
+//     console.log("Local Count is: ", localCount)
+
+// }
+
+// addToCount()
 
 
 
 
 //function with parameter
-function displayName(firstName, lastName) {
-    alert(firstName, lastName)
-}
+// function displayName(firstName, lastName ,age , height) {
+//     alert(firstName + " " + lastName + " " + age + " " + height)
+// }
 
-displayName("John", "Doe")
-displayName("Ade", "Johnson")
-
-
-
-
-//function with returns
-function getFullName(firstName, lastName) {
-    const fullName = firstName + " " + lastName
-    return fullName
-}
-
-let fullname = getFullName("John", "Doe")
-alert(fullname)
-
-
-
-
-//function expression
-const sayHello = function () {
-    console.log("Hello World")
-}
-
-sayHello()
+// // displayName("John", "Doe" , "39" , "124")
+// // displayName("Ade", "Johnson" , "23" , "145")
 
 
 
 
 
-//callback functions
-function displayUser(displayType, showFullName, showUserName) {
-    if (displayType == "full") {
-        showFullName()
-    } else {
-        showUserName()
-    }
-}
+// //function with returns
+// function getFullName(firstName, lastName) {
+//     const fullName = firstName + " " + lastName
+//     return fullName
+// }
 
-function showFullName() { alert("John Doe") }
-function showUserName() { alert("JohnDoe234") }
+// let fullname = getFullName("John", "Doe")
+// alert(fullname)
 
-displayUser("full", showFullName, showUserName)
+
+
+// Example
+
+// function addsTwoNumber(number1,number2 ){
+//     const sum = number1 + number1 
+//     return sum
+// }
+
+// const result1 = addsTwoNumber(20,30)
+// console.log(result1)
+
+
+
+ //function expression
+// const sayHello = function() {
+//     console.log("Hello World")
+// }
+
+// sayHello()
+
+
+
+
+
+// //callback functions
+// function displayUser(displayType, showFullName, showUserName) {
+//     if (displayType == "full") {
+//         showFullName()
+//     } else {
+//         showUserName()
+//     }
+// }
+
+// function showFullName() { alert("John Doe") }
+// function showUserName() { alert("JohnDoe234") }
+
+// displayUser("full", showFullName, showUserName)
 
 
 
 //Arrow functions
 let sum = (a, b) => a + b;
-let sayHelloWorld = () => alert("Hello World!");
+let sayHelloWorld = () => alert("Hello World!"); 
